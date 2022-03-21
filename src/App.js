e8a21ee4331c403b8f2865c0904511df6b41e0f5
Russell Fraze <russell.fraze@gmail.com>
2022-03-21 10:34:55 -0700

    crud operations added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { v4 as uuidV4} from 'uuid'
 import {useState} from 'react'
 import './App.css';
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackData from './data/FeedbackData'
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidV4()
+    setFeedback([newFeedback, ...feedback])
+  }
 
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
@@ -13,10 +21,13 @@ function App() {
     }
   }
 
+
   return (
     <>
       <Header />
       <div className="container">
+        <FeedbackForm handleAdd={addFeedback}/>
+        <FeedbackStats feedback={feedback}/>
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
       </div>
     </>
