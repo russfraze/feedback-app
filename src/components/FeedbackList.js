603b284a9421eaffd4d14279320cfdeda6ee7889
Russell Fraze <russell.fraze@gmail.com>
2022-03-21 13:04:48 -0700

    about page route added

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -1,17 +1,36 @@
+import { motion, AnimatePresence } from 'framer-motion'
 import PropTypes from 'prop-types'
 import FeedbackItem from "./FeedbackItem"
 
-function FeedbackList({feedback, handleDelete}) {
+function FeedbackList({ feedback, handleDelete }) {
     if (!feedback || feedback.length === 0) {
         return <p>No feedback to show</p>
     }
+
     return (
         <div className='feedback-list'>
-            {feedback.map((item) => (
-                <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
-            ))}
+            <AnimatePresence>
+                {feedback.map((item) => (
+                    <motion.div 
+                    key={item.id}
+                    initial={{opacity: 0}}
+                    animate={{opacity: 1}}
+                    exit={{opacity: 0}}
+                    >
+                        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+                    </motion.div>
+                ))}
+            </AnimatePresence>
         </div>
     )
+
+    // return (
+    //     <div className='feedback-list'>
+    //         {feedback.map((item) => (
+    //             <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
+    //         ))}
+    //     </div>
+    // )
 }
 
 //remember you can also specify the shape of the array (what properties are inside) and their proptypes as well
